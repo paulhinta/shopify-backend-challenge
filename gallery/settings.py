51c39ab168a0fdf088446fbb0b8f7586fd4c4a37
paulhinta <paul_hinta@outlook.com>
2020-12-27 16:40:01 -0800

    Picked pokemon images to include in gallery

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #third party
+    'photo.apps.PhotoConfig'
 ]
 
 MIDDLEWARE = [
@@ -99,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#####
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = 'media/'
+#####
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
