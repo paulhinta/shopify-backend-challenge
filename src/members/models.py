62ea83aed505e4a1dc82aab229ec11b8065bc114
paulhinta <paul_hinta@outlook.com>
2021-01-11 16:53:33 -0500

    Edited models save

--- src/members/models.py
+++ src/members/models.py
@@ -15,7 +15,7 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     def save(self, *args, **kwargs): #overriding the save function in order to rescale images (save space, etc.)
-        super().save()
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
