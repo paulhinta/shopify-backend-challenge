c67480ebf66dd04c937b6c5f2abc838fdf2757ec
paulhinta <paul_hinta@outlook.com>
2021-01-11 22:49:34 -0500

    Deployed heroku properly, need to reinitialize host

--- src/gallery/settings.py
+++ src/gallery/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #stored in file system, not in the database
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
